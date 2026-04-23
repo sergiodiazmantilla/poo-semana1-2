6210e6545bd3f996b2ea32db769522189f112fbb
Wistermundo Sergio Diaz Mantilla <sergiodmantilla@outlook.com>
2026-04-22 21:51:42 -0500

    Agregar Clase Estudiante

---
+++ Estudiante.java
@@ -0,0 +1,50 @@
+public class Estudiante {
+    // Atributos
+    private int codigo;
+    private String nombre;
+    private Double nota;
+
+    // Constructor vacío
+    public Estudiante(){}
+
+    // Constructor con parametros
+    public Estudiante(int codigo, String nombre, Double nota) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.nota = nota;
+    }
+
+    // Getter y Setter
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        // validacion
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        // validacion
+        this.nombre = nombre;
+    }
+
+    public Double getNota() {
+        return nota;
+    }
+
+    public void setNota(Double nota) {
+        // validacion
+        this.nota = nota;
+    }
+
+    // Metodos
+    public void mostrarInfo(){
+        System.out.println(
+            "Estudiante: " + nombre + "Nota: " + nota);
+    }
+}
